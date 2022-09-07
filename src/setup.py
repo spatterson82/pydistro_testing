924d2f8b86a7c0fbfb4471c363dd825475e61f19
slpatterson <stephen.patterson@terracon.com>
2022-09-07 15:42:19 -0400

    added recipe and actions config

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+   name='pydistro_testing',
+   version='1.3.2',
+   description='Testing python open source',
+   author='spatterson82',
+   author_email='spatterson82@hotmail.com',
+   packages=['pydistro_testing'],  #same as name
+)
