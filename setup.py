17ffe228202e9eebbe6d0c45b40aebe0dc6eaf65
slpatterson <stephen.patterson@terracon.com>
2022-09-08 10:01:36 -0400

    testing pypi alone

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
    name='pydistro_testing',
-   version='1.3.2',
+   version='1.3.10',
    description='Testing python open source',
    author='spatterson82',
    author_email='spatterson82@hotmail.com',
