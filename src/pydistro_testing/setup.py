d4400a3bbd6e01b0b37c079dcf75afe8ce719797
slpatterson <stephen.patterson@terracon.com>
2022-09-08 13:56:37 -0400

    added setup again

---
+++ src/pydistro_testing/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pydistro_testing',
+      version='1.0',
+      description='Testing open-source publishing',
+      author='Stephen Patterson',
+      author_email='spatterson82@hotmail.com',
+      url='https://github.com/spatterson82/pydistro_testing',
+      packages=['pydistro_testing']
+     )
